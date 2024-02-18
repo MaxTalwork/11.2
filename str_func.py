f7b6b9f3c63bd14b3782906c85242171fa6b2c4a
MaxTalwork <160335906+MaxTalwork@users.noreply.github.com>
2024-02-19 00:39:00 +0300

    Напишите вторую функцию. Добавили docstring к функции из Задания 2.

--- str_func.py
+++ str_func.py
@@ -1,3 +1,18 @@
 def get_upper():
+    """
+    Меняет все буквы на заглавные
+    """
     text = input('Введите текст: ')
     print(f'{text.upper()}')
+
+
+def get_title():
+    """
+    Меняет первые буквы на заглавные
+    """
+    text = input('Введите текст: ')
+    print(f'{text.title()}')
+
+get_upper()
+get_title()
+
